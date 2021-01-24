52003bf3b3ed112a9190973ae27e25b35bda560f
Kesa Shabarish <shabarish.shabbi@gmail.com>
2021-01-24 13:57:43 -0500

    added get for postList, fixed thecatch error

--- src/components/postsList.component.js
+++ src/components/postsList.component.js
@@ -1,11 +1,50 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+require('dotenv').config();
+
+const PostSchema = props => (
+<tr>
+    <td>{props.post.title}</td>
+    <td>{props.post.description}</td>
+    <td>{props.post.date}</td>
+    <td>
+    {/* <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deletePost(props.post._id) }}>Delete Post</a> */}
+    </td>
+</tr>
+)
 
 export default class PostsList extends Component {
-  render() {
-    return (
-      <div>
-        This is Posts List Component.
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        //Binding the "this" to the function
+        // this.deletePost = this.deletePost.bind(this)
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(process.env.REACT_APP_API_URL + `/posts`)
+            .then(response => {
+                console.log(`posts: `,response.data)
+                this.setState({
+                    posts: response.data
+                })
+            })
+            .catch((error) => {
+                console.log(`error: `,error)
+            })
+    }
+
+    render() {
+        return (
+        <div>
+            This is Posts List Component.
+        </div>
+        )
+    }
 }
